152c4d5f0563347178b1d02160c7ff3faf539a6d
MackRoe <elaine.music@students.makeschool.com>
2021-03-21 10:47:58 -0400

    listens for message from client

--- chat-node-socket/index.js
+++ chat-node-socket/index.js
@@ -4,9 +4,25 @@ const app = express()
 const server = require('http').createServer(app)
 const port = process.env.PORT || 3000
 
+// import socket.io library
+const io = require('socket.io')(server)
+
+// import path
+const path = require('path')
+app.use(express.static(path.join(__dirname + '/public')))
+
+
+
 // just to test the server
 app.get('/', (req, res) => {
   res.status(200).send('Working')
+})
+
+io.on('connection', socket => {
+  // listens for message from client
+  socket.on('chat', message => {
+    console.log('From client: ', message)
+  })
 })
 
 server.listen(port, () => {
--- chat-node-socket/public/script.js
+++ chat-node-socket/public/script.js
@@ -1,1 +1,12 @@
 const socket = io()
+
+const chat = document.querySelector('.chat-form')
+const Input = document.querySelector('.chat-input')
+
+
+chat.addEventListener(', event => {
+  event.preventDefault()
+  // send messages using sockets
+  socket.emit('chat', Input.value)
+  Input.value = ''
+})
