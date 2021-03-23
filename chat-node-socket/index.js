93ae54c092507e06e6cbf38e801d6b08fa5dd51e
MackRoe <elaine.music@students.makeschool.com>
2021-03-22 20:09:32 -0400

    app sends message to all users

--- chat-node-socket/index.js
+++ chat-node-socket/index.js
@@ -11,8 +11,6 @@ const io = require('socket.io')(server)
 const path = require('path')
 app.use(express.static(path.join(__dirname + '/public')))
 
-
-
 // just to test the server
 app.get('/', (req, res) => {
   res.status(200).send('Working');
@@ -21,14 +19,12 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('A User Connected');
     socket.on('chat', (message) => {
-        io.emit('chat', message);
+        console.log('Message from Client: ', message);
+        io.emit('chat', message)
     });
 
   // listens for message from client
-  socket.on('chat', message => {
-    console.log('From client: ', message)
-    io.emit('chat', message);
-  })
+
   socket.on('disconnect', () => {
       console.log('user disconnected');
   });
--- chat-node-socket/public/script.js
+++ chat-node-socket/public/script.js
@@ -1,6 +1,6 @@
 const socket = io("ws://localhost:3000")
 
-const messages = document.getElementById('messages')
+const messages = document.getElementById('message')
 // not sure if this needs added or not
 
 const chat = document.querySelector('.chat-form')
@@ -8,21 +8,25 @@ const Input = document.querySelector('.chat-input')
 
 const chatWindow = document.querySelector('.chat-window')
 
+
+
+chat.addEventListener('submit', event => {
+  event.preventDefault();
+  // send messages using sockets
+  socket.emit('chat', Input.value);
+  Input.value = '';
+});
+
 const renderMessage = message => {
+    console.log('Client side message:', message);
     const div = document.createElement('div')
     div.classList.add('render-message')
     div.innerText = message
     chatWindow.appendChild(div)
 }
 
-chat.addEventListener('submit', event => {
-  event.preventDefault()
-  // send messages using sockets
-  socket.emit('chat', Input.value)
-  Input.value = ''
-})
-
 socket.on('chat', message => {
   console.log('From server: ', message);
+  // * issue traced to this point
   renderMessage(message)
 })
